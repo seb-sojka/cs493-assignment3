c4aa112605bc798b0b9febc5af162d2d92db4d10
Sebastian <sojkas@oregonstate.edu>
2019-10-21 22:57:16 -0500

    Update could not fix put for slips

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const datastore = new Datastore();
 
 const BOAT = "Boat";
 
-const Slip = "Slip";
+const SLIP = "Slip";
 
-const boats = express.Router();
+const router = express.Router();
 
 app.use(bodyParser.json());
 
@@ -19,11 +19,18 @@ function fromDatastore(item){
     return item;
 }
 
-/* ------------- Begin Lodging Model Functions ------------- */
+/* ------------- Begin Model Functions ------------- */
+/* ----------- Boats -----------*/
 function post_boat(name, type, length){
     var key = datastore.key(BOAT);
 	const new_boat = {"name": name, "type": type, "length": length};
 	return datastore.save({"key":key, "data":new_boat}).then(() => {return key});
+}
+
+function patch_boat(id, name, type, length){
+    const key = datastore.key([BOAT, parseInt(id,10)]);
+    const boat = {"name": name, "type": type, "length": length};
+    return datastore.update({"key":key, "data":boat}).then(() => {return key});
 }
 
 function get_boats(){
@@ -33,56 +40,103 @@ function get_boats(){
 		});
 }
 
-function patch_boat(id, name, type, length){
-    const key = datastore.key([BOAT, parseInt(id,10)]);
-    const boat = {"name": name, "type": type, "length": length};
-    return datastore.update({"key":key, "data":boat}).then(() => {return key});
+function get_slips(){
+	const q = datastore.createQuery(SLIP);
+	return datastore.runQuery(q).then( (entities) => {
+			return entities[0].map(fromDatastore);
+		});
 }
 
-function delete_boat(id){
-    const key = datastore.key([BOAT, parseInt(id,10)]);
+function post_slip(number){
+    var key = datastore.key(SLIP);
+	const new_slip = {"number": number, "current_boat": null};
+	return datastore.save({"key":key, "data":new_slip}).then(() => {return key});
+}
+
+function boatArrives(slipID, boatID)
+{
+    var key = datastore.key([BOAT, parseInt(slipID,10)]);
+	const new_slip = {"number": number, "current_boat": boatID};
+	return datastore.save({"key":key, "data":new_slip}).then(() => {return key});
+}
+
+/*-------Universals---------*/
+function get_entities(type){
+	const q = datastore.createQuery(type);
+	return datastore.runQuery(q).then( (entities) => {
+			return entities[0].map(fromDatastore);
+		});
+}
+
+function delete_entity(id, type){
+    const key = datastore.key([type, parseInt(id,10)]);
     return datastore.delete(key); 
 }
 
-function get_boat(id)
+function get_entity(id, type)
 {
-    const key = datastore.key([BOAT, parseInt(id,10)]);
+    const key = datastore.key([type, parseInt(id,10)]);
     return datastore.get(key);
 }
 
-function createJSONResp(boat, idAdd, selfAdd)
+function createJSONResp(array, req, idAdd)
 {
-    var myJSON = boat[0];
+    var myJSON = array[0];
+    selfAdd = req.protocol + '://' + req.get('host') + req.path + '/';
     myJSON.id = idAdd;
     myJSON.self = selfAdd;
     return myJSON;
 }
 
-function createJSONRespArray(boats, selfAdd)
+function createJSONRespArray(array, req)
 {
     var myJSON = [];
-    for(i = 0; i < boats.length; i++)
+    selfAdd = req.protocol + '://' + req.get('host') + req.originalUrl + '/';
+    for(i = 0; i < array.length; i++)
     {
-        myJSON[i] = boats[i];
-        myJSON[i].self = selfAdd + myJSON[i].id
+        myJSON[i] = array[i];
+        myJSON[i].self = selfAdd + myJSON[i].id;
     }
     return myJSON;
+}
+
+function check_entity(id, type)
+{
+    var retVal = 0;
+    const key = datastore.key([type, parseInt(id,10)]);
+    const entity = datastore.get(key);
+    if(entity[0] == null)
+    {
+        retVal = 1;
+    }
+    else
+
+    return (retVal);
+}
+
+
+function slipBoatCheck(slip_id, boat_id)
+{
+    var retVal = check_entity(slip_id, SLIP);
+    retVal = check_entity(boat_id, boat)
+
+    return retVal;
 }
 
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Controller Functions ------------- */
 
-boats.get('/', function(req, res){
+/* -------Boats------ */
+
+router.get('/boats/', function(req, res){
     const boats = get_boats()
 	.then( (boats) => {
-        selfBase = req.protocol + '://' + req.get('host') + req.baseUrl + '/';
-
-        res.status(200).json(createJSONRespArray(boats, selfBase));
+        res.status(200).json(createJSONRespArray(boats, req));
     });
 });
 
-boats.post('/', function(req, res){
+router.post('/boats/', function(req, res){
 	if(req.body.name == null || req.body.type == null || req.body.length == null)
     {
 		res.status(400).send('{ "Error":  "The request object is missing at least one of the required attribute" }')
@@ -91,15 +145,14 @@ boats.post('/', function(req, res){
     {
         post_boat(req.body.name, req.body.type, req.body.length)
         .then( key => {
-            const boat = get_boat(key.id).then( (boat) => {
-                self = req.protocol + '://' + req.get('host') + req.baseUrl + '/' + key.id;
-                res.status(201).json(createJSONResp(boat, key.id, self));
+            const boat = get_entity(key.id, BOAT).then( (boat) => {
+                res.status(201).json(createJSONResp(boat, req, key.id));
             });
         });
     }
 });
 
-boats.patch('/:id', function(req, res){
+router.patch('/boats/:id', function(req, res){
     if(req.body.name == null || req.body.type == null || req.body.length == null)
     {
 		res.status(400).json({Error:  "The request object is missing at least one of the required attribute"})
@@ -108,44 +161,93 @@ boats.patch('/:id', function(req, res){
     {
         patch_boat(req.params.id, req.body.name, req.body.type, req.body.length)
         .then(key => {
-            const boat = get_boat(key.id).then( (boat) => {
+            const boat = get_entities(key.id, BOAT).then( (boat) => {
                 self = req.protocol + '://' + req.get('host') + req.baseUrl + '/' + key.id;
-                res.status(200).json(createJSONResp(boat, key.id, self));
+                res.status(200).json(createJSONResp(boat, req, key.id));
             });
         });
     }
 });
 
-boats.get('/:id', function(req, res){
-    const boat = get_boat(req.params.id).then( (boat) => {
+router.get('/boats/:id', function(req, res){
+    const boat = get_entity(req.params.id, BOAT).then( (boat) => {
         if(boat[0] == null)
         {
             res.status(404).json({Error:  "No boat with this boat_id exists"});
         }
         else
         {
-            self = req.protocol + '://' + req.get('host') + req.baseUrl + '/' + req.params.id;
-            res.status(200).json(createJSONResp(boat, req.params.id, self));
+            res.status(200).json(createJSONResp(boat, req, req.params.id));
         }
     } );
 });
 
-boats.delete('/:id', function(req, res){
-    const boat = get_boat(req.params.id).then( (boat) => {
+router.delete('/boats/:id', function(req, res){
+    const boat = get_entity(req.params.id, BOAT).then( (boat) => {
     if(boat[0] == null)
     {
         res.status(404).json({Error:  "No boat with this boat_id exists"});
     }
     else
     {
-        delete_boat(req.params.id).then(res.status(200).end());
+        delete_entity(req.params.id, BOAT).then(res.status(204).end());
     }
     });
 });
 
+/*-------------slips ------------ */
+router.get('/slips/', function(req, res){
+    const slips = get_slips()
+	.then( (slips) => {
+        res.status(200).json(createJSONRespArray(slips, req));
+    });
+});
+
+router.post('/slips/', function(req, res){
+	if(req.body.number == null)
+    {
+		res.status(400).send('{ "Error":  "The request object is missing at least one of the required attribute" }')
+    }
+   else
+    {
+        post_slip(req.body.number)
+        .then( key => {
+            const slip = get_entity(key.id, SLIP).then( (slip) => {
+                res.status(201).json(createJSONResp(slip, req, key.id));
+            });
+        });
+    }
+});
+
+router.delete('/slips/:id', function(req, res){
+    const slip = get_entity(req.params.id, SLIP).then( (slip) => {
+    if(slip[0] == null)
+    {
+        res.status(404).json({Error:  "No slip with this slip_id exists"});
+    }
+    else
+    {
+        delete_entity(req.params.id, SLIP).then(res.status(204).end());
+    }
+    });
+});
+
+router.get('/slips/:slip_id', function(req, res){
+    const slip = get_entity(req.params.slip_id, SLIP).then( (slip) => {
+        if(slip[0] == null)
+        {
+            res.status(404).json({Error:  "No slip with this slip_id exists"});
+        }
+        else
+        {
+            res.status(200).json(createJSONResp(slip, req, req.params.slip_id));
+        }
+    } );
+});
+
 /* ------------- End Controller Functions ------------- */
 
-app.use('/boats', boats);
+app.use('/', router);
 
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
